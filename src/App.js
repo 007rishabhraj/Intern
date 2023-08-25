17cc50362ade14342a9fa97afee28f85d1a2c4f2
007rishabhraj <rishabhraj135007@gmail.com>
2023-08-25 16:27:25 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Image from "./component/Image";
 import TodoList from "./component/TodoList";
+import Header from "./component/Header";
 
 const App = () => {
   return (
     <>
+    <Header/>
       <Image />
       <TodoList />
     </>
